3ced89856dc00d857d55853b393638a7448dff9b
mabyoungg <mabyoungg@gmail.com>
2024-04-04 19:57:43 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/example/springbooturl/SpringBootUrlApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbooturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUrlApplication.class, args);
+    }
+
+}
