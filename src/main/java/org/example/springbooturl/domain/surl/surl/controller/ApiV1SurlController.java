c977cfeddc5d7fc8d3301890de4aa0bd1f096486
mabyoungg <mabyoungg@gmail.com>
2024-04-06 18:46:46 +0900

    feat: AuthenticationFilter 추가, 임시 인증 객체 설정

--- src/main/java/org/example/springbooturl/domain/surl/surl/controller/ApiV1SurlController.java
+++ src/main/java/org/example/springbooturl/domain/surl/surl/controller/ApiV1SurlController.java
@@ -5,14 +5,18 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.springbooturl.domain.member.member.entity.Member;
 import org.example.springbooturl.domain.member.member.service.MemberService;
 import org.example.springbooturl.domain.surl.surl.service.SurlService;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("/api/v1/surls")
 @RequiredArgsConstructor
+@Slf4j
 public class ApiV1SurlController {
     private final SurlService surlService;
     private final MemberService memberService;
@@ -27,8 +31,10 @@ public class ApiV1SurlController {
 
     @PostMapping("")
     public void create(
-            @Valid @RequestBody SurlCreateReqBody reqBody
+            @Valid @RequestBody SurlCreateReqBody reqBody,
+            Principal principal
     ) {
+        log.debug("principal: {}", principal);
         Member author = memberService.findById(4L).get();
         surlService.create(author, reqBody.url, reqBody.title);
     }
