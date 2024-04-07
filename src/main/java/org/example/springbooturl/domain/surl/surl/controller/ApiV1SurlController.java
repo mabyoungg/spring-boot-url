322aba052088376549a2dafe28fdb0692f5bb7fd
mabyoungg <mabyoungg@gmail.com>
2024-04-07 20:29:11 +0900

    refactor: Rq SecurityUser

--- src/main/java/org/example/springbooturl/domain/surl/surl/controller/ApiV1SurlController.java
+++ src/main/java/org/example/springbooturl/domain/surl/surl/controller/ApiV1SurlController.java
@@ -6,12 +6,11 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.springbooturl.domain.member.member.entity.Member;
 import org.example.springbooturl.domain.member.member.service.MemberService;
 import org.example.springbooturl.domain.surl.surl.service.SurlService;
-import org.example.springbooturl.global.security.SecurityUser;
+import org.example.springbooturl.global.rq.Rq;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 public class ApiV1SurlController {
     private final SurlService surlService;
     private final MemberService memberService;
+    private final Rq rq;
 
     @Data
     public static class SurlCreateReqBody {
@@ -32,15 +32,11 @@ public class ApiV1SurlController {
 
     @PostMapping("")
     @PreAuthorize("isAuthenticated()")
+    @Transactional
     public void create(
-            @Valid @RequestBody SurlCreateReqBody reqBody,
-            @AuthenticationPrincipal SecurityUser user
+            @Valid @RequestBody SurlCreateReqBody reqBody
     ) {
-        Member authMember = memberService.findById(user.getId()).get();
-        log.debug("user: {}", user);
-
-        Member author = authMember;
-        surlService.create(author, reqBody.url, reqBody.title);
+        surlService.create(rq.getMember(), reqBody.url, reqBody.title);
     }
 
     @Data
