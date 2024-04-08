d5dcf89711627ff53b21b14f0bdf7dbe9f221e88
mabyoungg <mabyoungg@gmail.com>
2024-04-09 00:24:45 +0900

    feat: EnableMethodSecurity annotation 추가

--- src/main/java/org/example/springbooturl/global/security/SecurityConfig.java
+++ src/main/java/org/example/springbooturl/global/security/SecurityConfig.java
@@ -3,12 +3,14 @@ package org.example.springbooturl.global.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private final AuthenticationFilter authenticationFilter;
 
